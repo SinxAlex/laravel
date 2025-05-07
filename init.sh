1791fdf7dba6d84b0c2a11d510247548e6a003e6
Alex <aleshaascet@gmail.com>
2025-05-07 13:30:38 +0300

    change Dockerfile and init.sh

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ cd /var/www
 # Если .env не существует, создаем его
 if [ ! -f .env ]; then
     cat > .env <<EOF
-APP_KEY=
+
 DB_CONNECTION=pgsql
 DB_HOST=db
 DB_PORT=5432
@@ -14,11 +14,26 @@ DB_DATABASE=laravel
 DB_USERNAME=laravel
 DB_PASSWORD=secret
 EOF
-
-    # Генерируем ключ только если .env только что создан
-    php artisan key:generate --force
 fi
-
-
 exec "$@"
 
+# init.sh
+#!/bin/bash
+set -e
+
+cd /var/www
+
+# Если .env не существует, создаем его
+if [ ! -f .env ]; then
+    cat > .env <<EOF
+
+DB_CONNECTION=pgsql
+DB_HOST=db
+DB_PORT=5432
+DB_DATABASE=laravel
+DB_USERNAME=laravel
+DB_PASSWORD=secret
+EOF
+fi
+
+exec "$@"
