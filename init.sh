db29ebedb64d54646f6e6e9d5da7529c018f7756
Alex <aleshaascet@gmail.com>
2025-05-07 09:41:36 +0300

    npm build

--- init.sh
+++ init.sh
@@ -1,4 +1,24 @@
 #!/bin/bash
-docker-compose exec app php artisan db:seed --class=UserSeeder
+set -e
 
+cd /var/www
+
+# Если .env не существует, создаем его
+if [ ! -f .env ]; then
+    cat > .env <<EOF
+APP_KEY=
+DB_CONNECTION=pgsql
+DB_HOST=db
+DB_PORT=5432
+DB_DATABASE=laravel
+DB_USERNAME=laravel
+DB_PASSWORD=secret
+EOF
+
+    # Генерируем ключ только если .env только что создан
+    php artisan key:generate --force
+fi
+
+
+exec "$@"
 
