460dc00fd25f8341309cdfb45b8b32c4ada769dc
Alex <aleshaascet@gmail.com>
2025-05-07 14:29:17 +0300

    change init.sh

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ cd /var/www
 # Если .env не существует, создаем его
 if [ ! -f .env ]; then
     cat > .env <<EOF
-
+APP_KEY=
 DB_CONNECTION=pgsql
 DB_HOST=db
 DB_PORT=5432
@@ -15,25 +15,4 @@ DB_USERNAME=laravel
 DB_PASSWORD=secret
 EOF
 fi
-exec "$@"
-
-# init.sh
-#!/bin/bash
-set -e
-
-cd /var/www
-
-# Если .env не существует, создаем его
-if [ ! -f .env ]; then
-    cat > .env <<EOF
-
-DB_CONNECTION=pgsql
-DB_HOST=db
-DB_PORT=5432
-DB_DATABASE=laravel
-DB_USERNAME=laravel
-DB_PASSWORD=secret
-EOF
-fi
-
 exec "$@"
